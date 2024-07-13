f78a230a9c3665788f38fe9f56b32045064ae2d5
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-07-13 13:12:02 +1000

    setup: React vite and tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-blue-100 grid place-items-center">
+      <div className="text-2xl font-bold">Hello there!</div>
+    </div>
+  )
+}
+
+export default App

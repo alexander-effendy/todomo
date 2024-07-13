a0bbeab1c60774d648f93b45041d7df90a8f5054
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-07-13 13:34:01 +1000

    setup: sahdcn

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,6 +1,29 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
+
 const Home = () => {
   return (
-    <div>This is home</div>
+    <div>
+      This is home
+      <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
+
+    </div>
   )
 }
 
